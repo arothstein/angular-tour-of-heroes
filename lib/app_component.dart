3ac54c1992920f3172d10fe575f7aa6c202e8e4e
Aaron Rothstein <aaron.rothstein@wunita.com>
2018-01-20 22:46:01 -0600

    Tutorial: Step 2 progress

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,13 +1,18 @@
 import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
 import 'src/hero.dart';
+import 'src/mock_heroes.dart';
 
 @Component(
   selector: 'my-app',
   templateUrl: 'app_component.html',
-  directives: const [formDirectives],
+  styleUrls: const ['app_component.css'],
+  directives: const [CORE_DIRECTIVES, formDirectives],
 )
 class AppComponent {
   final title = 'Tour of Heroes';
-  Hero hero = new Hero(1,'Windstorm');
+  List<Hero> heroes = mockHeroes;
+  Hero selectedHero;
+
+  void onSelect(Hero hero) => selectedHero = hero;
 }
