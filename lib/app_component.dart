e1f9e6e449887f8bac28765b13c2d39be5232160
Aaron Rothstein <aaron.rothstein@wunita.com>
2018-01-20 23:23:21 -0600

    Tutorial Step 4 progress

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,19 +1,28 @@
 import 'package:angular/angular.dart';
 
 import 'src/hero.dart';
-import 'src/mock_heroes.dart';
 import 'src/hero_detail_component.dart';
+import 'src/hero_service.dart';
 
 @Component(
   selector: 'my-app',
   templateUrl: 'app_component.html',
   styleUrls: const ['app_component.css'],
   directives: const [CORE_DIRECTIVES, HeroDetailComponent],
+  providers: const [HeroService],
 )
-class AppComponent {
+class AppComponent implements OnInit{
   final title = 'Tour of Heroes';
-  List<Hero> heroes = mockHeroes;
+  List<Hero> heroes;
   Hero selectedHero;
+  final HeroService _heroService;
+  AppComponent(this._heroService);
+
+  void ngOnInit() => getHeroes();
 
   void onSelect(Hero hero) => selectedHero = hero;
+
+  Future<Null> getHeroes() async {
+    heroes = await _heroService.getHeroes();
+  }
 }
